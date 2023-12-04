59f247f97b99f59713d82b70d15fd134605ccad1
ducbang02 <nguyenducbang.uit@gmail.com>
2023-12-05 05:28:54 +0700

    fix lỗi

--- main.py
+++ main.py
@@ -4,34 +4,34 @@ import numpy as np
 
 st.title('Phân loại bệnh nhân tiểu đường')
 
-# # Load the model
-# input_model = open('test.pkl', 'rb')
-# model = pkl.load(input_model)
+# Load the model
+input_model = open('test.pkl', 'rb')
+model = pkl.load(input_model)
 
-# # Streamlit app
+# Streamlit app
 
-# st.title("Ứng Dụng Dự Đoán Điabetes")
+st.title("Ứng Dụng Dự Đoán Điabetes")
 
-# # Tạo 8 text fields và lưu giá trị vào mảng input_data
-# input_data = []
-# for i in range(1, 9):
-#     value = st.text_input(f"Nhập giá trị {i}:", key=f"value{i}")
-#     if value:
-#         input_data.append(float(value))
+# Tạo 8 text fields và lưu giá trị vào mảng input_data
+input_data = []
+for i in range(1, 9):
+    value = st.text_input(f"Nhập giá trị {i}:", key=f"value{i}")
+    if value:
+        input_data.append(float(value))
 
-# # Kiểm tra nếu đã nhập đủ 8 giá trị
-# if len(input_data) == 8:
-#     st.success("Đã nhập đủ số lượng.")
-#     input_data = np.asarray(input_data).reshape(1, -1)  # Reshape thành mảng 2 chiều
-#     # Thực hiện dự đoán
-#     prediction = classifier.diabetes(input_data)
-#     # Hiển thị kết quả
-#     st.write("Kết quả dự đoán:")
-#     if (prediction[0] == 0):
-#       st.write('The person is not diabetic')
-#     else:
-#       st.write('The person is diabetic')
+# Kiểm tra nếu đã nhập đủ 8 giá trị
+if len(input_data) == 8:
+    st.success("Đã nhập đủ số lượng.")
+    input_data = np.asarray(input_data).reshape(1, -1)  # Reshape thành mảng 2 chiều
+    # Thực hiện dự đoán
+    prediction = classifier.diabetes(input_data)
+    # Hiển thị kết quả
+    st.write("Kết quả dự đoán:")
+    if (prediction[0] == 0):
+      st.write('The person is not diabetic')
+    else:
+      st.write('The person is diabetic')
     
-# else:
-#     st.warning("Hãy nhập đủ số lượng.")
+else:
+    st.warning("Hãy nhập đủ số lượng.")
 
